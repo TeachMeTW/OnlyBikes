8a69354c97d4f36dcd5c0d7ebaa667aa850758c3
Shizuka <tkpp26@gmail.com>
2022-11-05 09:56:19 -0700

    added user model

--- onlyBikes/onlyBikesApp/models.py
+++ onlyBikes/onlyBikesApp/models.py
@@ -8,10 +8,23 @@ class BikeModel(models.Model):
     brand_name = models.CharField(max_length = 100)
     description = models.CharField(max_length = 500)
     
+    
     COND = (
         ('N', 'New'),
         ('LN', 'Like-New'),
         ('U', 'Used'),
     )
     condition = models.CharField(max_length = 2, choices = COND)
-    
+
+class User(models.Model):
+    username = models.CharField(max_length = 200)
+    email = models.EmailField(max_length = 200)
+    password = models.CharField(max_length = 200)
+    bike_model = models.ManyToManyField(BikeModel)
+    startRental = models.DateTimeField()
+    endRental = models.DateTimeField()
+
+
+
+
+
