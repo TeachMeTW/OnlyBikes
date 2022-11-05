5a88c7234651a1f2a5df9bd303512488e513e485
TeachMeTW <teachmetheways652@gmail.com>
2022-11-04 18:31:47 -0700

    added bike model

--- onlyBikes/onlyBikes/urls.py
+++ onlyBikes/onlyBikes/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('onlyBikesApp/', include('onlyBikesApp.urls')),
+    path('', include('onlyBikesApp.urls')),
     path('admin/', admin.site.urls),
 ] 
--- onlyBikes/onlyBikesApp/models.py
+++ onlyBikes/onlyBikesApp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class BikeModel(models.Model):
+    
+    price = models.IntegerField()
+    model_name = models.CharField(max_length = 200)
+    brand_name = models.CharField(max_length = 100)
+    description = models.CharField(max_length = 500)
+    
+    COND = (
+        ('N', 'New'),
+        ('LN', 'Like-New'),
+        ('U', 'Used'),
+    )
+    condition = models.CharField(max_length = 2, choices = COND)
+    
