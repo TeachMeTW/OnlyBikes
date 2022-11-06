13538a1871b9888bd44984de7c9ced9ccdc015bd
Eric Pineda <ericvpineda@berkeley.edu>
2022-11-05 20:05:08 -0700

    cloned -- robins branch

--- onlyBikes/onlyBikes/urls.py
+++ onlyBikes/onlyBikes/urls.py
@@ -17,7 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('', include('onlyBikesApp.urls')),
-    path('', include('social_django.urls')),
+    path('onlyBikesApp/', include('onlyBikesApp.urls')),
     path('admin/', admin.site.urls),
 ] 
--- onlyBikes/onlyBikesApp/urls.py
+++ onlyBikes/onlyBikesApp/urls.py
@@ -2,8 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path("index/", views.index, name="index"),
+    path("", views.index, name="index"),
     path("login", views.login, name="login"),
     path("logout", views.logout, name="logout"),
     path("callback", views.callback, name="callback"),
