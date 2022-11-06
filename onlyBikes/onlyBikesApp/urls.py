9aa913bcfbec5182b65a7759cde67a5772dd8e3d
Eric Pineda <ericvpineda@berkeley.edu>
2022-11-05 20:47:51 -0700

    fixed bug -- commented out strings settings.py

--- onlyBikes/onlyBikesApp/urls.py
+++ onlyBikes/onlyBikesApp/urls.py
@@ -2,12 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.home, name='home'),
     path('index/', views.index, name='index'),
     path('temp/', views.temp, name='temp'),
     path('show/', views.show, name='show'),
     path('logout/', views.logout, name ='logout'),
     path('profile/', views.profile, name ='profile'),
-    path('', views.home, name='home'),
-
     # REMOVE THIS
 ]
